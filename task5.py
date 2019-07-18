250c0723868a0a49d6d617b217ba61c60dfef1a3
efyudi <fakharudin99@gmail.com>
2019-07-18 14:47:34 +0500

    TASK :  UPDATED

--- task5.py
+++ task5.py
@@ -1,11 +1,14 @@
-def multiplies(limit):
+def multiplies(limit, divisors):
     sum = 0
     for i in range(0, limit + 1):
-        if divmod(i, 3)[1] == 0 or divmod(i, 5)[1] == 0:
-            sum += i
+        for j in divisors:
+            if divmod(i, j)[1] == 0:
+                sum += i
+                break
     return sum
 
 
 if __name__ == "__main__":
+    divisors = [2, 5, 10, 13, 11]
     limit = int(input("Enter a Number : "))
-    print("Sum of Multiplies : ", multiplies(limit))
+    print("Sum of Multiplies : ", multiplies(limit, divisors))
